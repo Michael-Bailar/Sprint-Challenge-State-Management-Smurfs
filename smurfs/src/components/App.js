b26ef068f31a04ead8c22aacf5f7cf8b9342f142
Michael-B1764 <mikebailar@gmail.com>
2020-03-20 11:36:11 -0700

    form to add smurf works. nowIjust need to set initial state

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,44 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
+import { reducer } from '../reducers/reducer'
+import Villiage from './Villiage'
+import Header from './Header'
+import SmurfForm from "./SmurfForm";
+
+
+const store = createStore(reducer, applyMiddleware(thunk))
+
+
+const App = props => {
+  return (
+    <Provider store={store}>
+    <div className="App">
+      <Header />
+      <SmurfForm />
+      <h2>Smurf City</h2>
+      <div className='villiage-container'>
+        <Villiage />
       </div>
-    );
-  }
+    </div>
+  </Provider>
+  );
 }
 
+
 export default App;
+
+
+
+/*
+I can set the initial state as an empty array.  same as the card app. 
+When I load the app, I need to get it to load the initial state
+THEN grab the data from the server and set that to state. 
+(JUST NEED TO CONNECT EVERYTHING HERE)
+I need to add a form that adds a smurf to the smurfs state.
+
+*/
---
+++ smurfs/src/components/SmurfForm.js
@@ -0,0 +1,96 @@
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+
+import { addSmurf } from '../actions/actions'
+
+
+
+const SmurfForm = props => {
+
+    const [newSmurf, setNewSmurf] = useState({
+        name: '',
+        age: '',
+        height: ''
+    })
+
+    const handleChanges = e => {
+        setNewSmurf({
+            ...newSmurf,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const addNewSmurf = smurf => {
+        const newSmurf = {
+            name: smurf.name,
+            age: smurf.age,
+            role: smurf.role
+        }
+        setNewSmurf({newSmurf})
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        addNewSmurf(newSmurf)
+        console.log(newSmurf)
+        props.addSmurf(newSmurf)
+    }
+
+
+    return (
+        <div className='smurf-form-container'>
+            <p>Add a Smurf</p>
+            
+            <form onSubmit={handleSubmit} className='smurf-form'>
+            <div className='smurf-form-inner-container'>
+                <label>
+                    Name:
+                <input
+                        id='name'
+                        type="text"
+                        name="name"
+                        value={newSmurf.name}
+                        onChange={handleChanges}
+                        placeholder="name"
+                    />
+                </label>
+                <label>
+                    Age:
+                <input
+                        id='age'
+                        type="number"
+                        name="age"
+                        value={newSmurf.age}
+                        onChange={handleChanges}
+                        placeholder="age"
+                    />
+                </label>
+                <label>
+                    Height(in cm):
+            <input
+                        id='height'
+                        type="text"
+                        name="height"
+                        value={newSmurf.height}
+                        onChange={handleChanges}
+                        placeholder="height"
+                    />
+                </label>
+                
+                <br />
+                </div>
+                <button type="submit">Add Smurf</button>
+            </form>  
+        </div> 
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        ...state
+    }
+}
+export default connect(
+    mapStateToProps,
+    { addSmurf }
+)(SmurfForm)
